cc5864fc1d8306f69bb99d2e32b6bb1d092e5f69
huongduyen3012 <duyen.vanthi.cit20@eiu.edu.vn>
2025-01-17 20:25:20 +0700

    feat(init): update fe

---
+++ Lab1_VanThiHuongDuyen/ChatApplication/App.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/no-unstable-nested-components */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import GroupScreen from './code/GroupScreen';
+import HomeScreen from './code/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="Group CSE 434" component={GroupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
